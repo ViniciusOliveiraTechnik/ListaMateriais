f9b690126ec9f4b8ae05ed93da90f267b778ead4
ViniciusOliveiraTechnik <vinicius.oliveira@grupotechnik.com.br>
2025-01-28 16:19:05 -0300

    Arrumando as settings do projeto

--- automacao_lista_materiais/settings.py
+++ automacao_lista_materiais/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -101,15 +101,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Diretório para arquivos coletados
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Diretórios com arquivos estáticos
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
