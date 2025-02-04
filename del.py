5d5f4fe86fd19e6c5a50edd9ec35a9b5f0035e36
unknown <vinicius.oliveira@grupotechnik.com.br>
2025-02-04 07:15:56 -0300

    Deletando arquivos temporários

--- del.py
+++
@@ -1,5 +0,0 @@
-required_columns = ['Long Description (Family)', 'Spec', 'Size', 'Fixed Length']
-
-index = required_columns.index('Fixed Length')
-required_columns.pop(index)
-print(required_columns)
